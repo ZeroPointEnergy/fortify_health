6ce86fde28a1dd2305a25cc8642ea0366db79f4f
Andreas Zuber <zuber@puzzle.ch>
2018-07-12 18:15:03 +0200

    selection with search

--- src/controllers/side_dish_controller.cr
+++ src/controllers/side_dish_controller.cr
@@ -13,68 +13,67 @@ class SideDishController < ApplicationController
   end
 
   def new
-    with_meal do |meal|
-      side_dish = SideDish.new
+    user = get_user
+    meal = resolve(params["meal_id"], Meal)
+    side_dish = SideDish.new
+    render("new.slang")
+  end
+
+  def create
+    user = get_user
+    meal = resolve(params["meal_id"], Meal)
+    side_dish = SideDish.new(side_dish_params.validate!)
+    side_dish.meal_id = meal.id
+
+    if side_dish.valid? && side_dish.save
+      flash["success"] = "Created SideDish successfully."
+      meal.update_nutrition_facts
+      redirect_to "/meals/#{meal.id}"
+    else
+      flash["danger"] = "Could not create SideDish!"
       render("new.slang")
     end
   end
 
-  def create
-    with_meal do |meal|
-      side_dish = SideDish.new(side_dish_params.validate!)
-      side_dish.meal_id = meal.id
-
-      if side_dish.valid? && side_dish.save
-        flash["success"] = "Created SideDish successfully."
-        meal.update_nutrition_facts
-        redirect_to "/meals/#{meal.id}"
-      else
-        flash["danger"] = "Could not create SideDish!"
-        render("new.slang")
-      end
-    end
-  end
-
   def edit
-    with_meal do |meal|
-      if side_dish = SideDish.find params["id"]
-        render("edit.slang")
-      else
-        flash["warning"] = "SideDish with ID #{params["id"]} Not Found"
-        redirect_to "/meals/#{meal.id}"
-      end
+    user = get_user
+    meal = resolve(params["meal_id"], Meal)
+    if side_dish = SideDish.find params["id"]
+      render("edit.slang")
+    else
+      flash["warning"] = "SideDish with ID #{params["id"]} Not Found"
+      redirect_to "/meals/#{meal.id}"
     end
   end
 
   def update
-    with_meal do |meal|
-      if side_dish = SideDish.find(params["id"])
-        side_dish.set_attributes(side_dish_params.validate!)
-        if side_dish.valid? && side_dish.save
-          flash["success"] = "Updated SideDish successfully."
-          meal.update_nutrition_facts
-          redirect_to "/meals/#{meal.id}"
-        else
-          flash["danger"] = "Could not update SideDish!"
-          render("edit.slang")
-        end
-      else
-        flash["warning"] = "SideDish with ID #{params["id"]} Not Found"
+    user = get_user
+    meal = resolve(params["meal_id"], Meal)
+    if side_dish = SideDish.find(params["id"])
+      side_dish.set_attributes(side_dish_params.validate!)
+      if side_dish.valid? && side_dish.save
+        flash["success"] = "Updated SideDish successfully."
+        meal.update_nutrition_facts
         redirect_to "/meals/#{meal.id}"
+      else
+        flash["danger"] = "Could not update SideDish!"
+        render("edit.slang")
       end
+    else
+      flash["warning"] = "SideDish with ID #{params["id"]} Not Found"
+      redirect_to "/meals/#{meal.id}"
     end
   end
 
   def destroy
-    with_meal do |meal|
-      if side_dish = SideDish.find params["id"]
-        side_dish.destroy
-        meal.update_nutrition_facts
-      else
-        flash["warning"] = "SideDish with ID #{params["id"]} Not Found"
-      end
-      redirect_to "/meals/#{meal.id}"
+    meal = resolve(params["meal_id"], Meal)
+    if side_dish = SideDish.find params["id"]
+      side_dish.destroy
+      meal.update_nutrition_facts
+    else
+      flash["warning"] = "SideDish with ID #{params["id"]} Not Found"
     end
+    redirect_to "/meals/#{meal.id}"
   end
 
   def side_dish_params
